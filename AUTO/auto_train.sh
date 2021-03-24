af1b14fb5d68191f92368f524dde05ea9282aba7
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2021-03-24 14:12:07 +0800

    Update and rename test.sh to AUTO/auto_train.sh

---
+++ AUTO/auto_train.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# $1 train.py 路径
+python3 ../train.py &
+while(true)
+do
+	tk=`curl -s http://localhost:5000/get_tk`
+	if [ "$tk"x != "$tk_o"x ]; then
+		sed -i 's/tk =.*/tk = '\"$tk\"'/' ../TickerConfig.py
+		killall python3
+		python3 ../train.py &
+		python3 ../run.py r &
+		tk_o="$tk"
+	fi
+	sleep 60
+done
