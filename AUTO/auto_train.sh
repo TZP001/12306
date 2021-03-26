2cfc8b976ab643463afcd8ba4c0ff0b9f5b43431
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2021-03-26 14:09:05 +0800

    Update auto_train.sh

--- AUTO/auto_train.sh
+++ AUTO/auto_train.sh
@@ -6,12 +6,26 @@ do
 	tk=`curl -s http://localhost:5000/get_tk`
 	rd=`curl -s http://localhost:5000/get_rd`
 	re=`curl -s http://localhost:5000/get_re`
+	change=false
 	if [ "$tk"x != "$tk_o"x ]; then
 		sed -i 's/tk =.*/tk = '\"$tk\"'/' ../TickerConfig.py
+		change=true
+		tk_o="$tk"
+	fi
+	if [ "$rd"x != "$rd_o"x ]; then
+		sed -i 's/RAIL_DEVICEID = .*/RAIL_DEVICEID = '\"$rd\"'/' ../TickerConfig.py
+		change=true
+		rd_o="$rd"
+	fi
+	if [ "$re"x != "$re_o"x ]; then
+		sed -i 's/RAIL_EXPIRATION =.*/RAIL_EXPIRATION = '\"$re\"'/' ../TickerConfig.py
+		change=true
+		re_o="$re"
+	fi
+	if [ $change = true ]; then
 		killall python3
 		python3 ../train.py &
 		python3 ../run.py r &
-		tk_o="$tk"
 	fi
 	sleep 60
 done
