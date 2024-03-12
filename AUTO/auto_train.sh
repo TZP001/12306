8d3f40c211a95f4912330c81af01f729e58abc77
TZP001 <145636057+TZP001@users.noreply.github.com>
2024-03-12 14:38:40 +0800

    Update auto_train.sh

--- AUTO/auto_train.sh
+++ AUTO/auto_train.sh
@@ -1,31 +1,41 @@
 #!/bin/sh
 # $1 train.py 路径
-python3 ../train.py &
+killall python3
+python3 $1/AUTO/train.py &
+
+sleep 5
+tk=`curl -s http://localhost:5000/get_tk`
+sed -i 's/tk =.*/tk = '\"$tk\"'/' $1/TickerConfig.py
+python3 $1/run.py r &
+
 while(true)
 do
 	tk=`curl -s http://localhost:5000/get_tk`
-	rd=`curl -s http://localhost:5000/get_rd`
-	re=`curl -s http://localhost:5000/get_re`
-	change=false
+#	rd=`curl -s http://localhost:5000/get_uk`
+#	re=`curl -s http://localhost:5000/get_je`
+	change="false"
 	if [ "$tk"x != "$tk_o"x ]; then
-		sed -i 's/tk =.*/tk = '\"$tk\"'/' ../TickerConfig.py
-		change=true
+		sed -i 's/tk =.*/tk = '\"$tk\"'/' $1/TickerConfig.py
+		change="true"
+		echo "修改文件"
 		tk_o="$tk"
 	fi
-	if [ "$rd"x != "$rd_o"x ]; then
-		sed -i 's/RAIL_DEVICEID = .*/RAIL_DEVICEID = '\"$rd\"'/' ../TickerConfig.py
-		change=true
-		rd_o="$rd"
-	fi
-	if [ "$re"x != "$re_o"x ]; then
-		sed -i 's/RAIL_EXPIRATION =.*/RAIL_EXPIRATION = '\"$re\"'/' ../TickerConfig.py
-		change=true
-		re_o="$re"
-	fi
-	if [ $change = true ]; then
+#	if [ "$rd"x != "$rd_o"x ]; then
+#		sed -i 's/uKey = .*/uKey = '\"$rd\"'/' $1/TickerConfig.py
+#		change=true
+#		rd_o="$rd"
+#	fi
+#	if [ "$re"x != "$re_o"x ]; then
+#		sed -i 's/JSESSIONID =.*/JSESSIONID = '\"$re\"'/' $1/TickerConfig.py
+#		change=true
+#		re_o="$re"
+#	fi
+
+	if [ $change = "true" ]; then
 		killall python3
-		python3 ../train.py &
-		python3 ../run.py r &
+		python3 $1/AUTO/train.py &
+		echo "重新运行"
+		python3 $1/run.py r &
 	fi
 	sleep 30
 done
